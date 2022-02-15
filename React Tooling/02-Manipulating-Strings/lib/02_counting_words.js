7541bd07ae0cafa5190608249b71f9cea649159f
coalva94 <coalva.94@gmail.com>
2022-02-15 11:30:06 -0500

    cambios

--- React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -2,9 +2,10 @@ function occurrences(text, word) {
   const a = text.split(" ");
   let count = 0;
   for (let i = 0; i < a.length; i++) {
-    if (word === (a[i].toLowerCase()))
+    if (word === (a[i].toLowerCase())) {
       count++;
+    }
+    return count;
   }
-  return count;
 }
 module.exports = occurrences;
--- React Tooling/03-ES6-Classes/lib/01_counter.js
+++ React Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,10 +1,19 @@
 class Counter {
   constructor(text) {
     // TODO: build an internal Map of word => occurrences.
+    this.map = new Map();
+    const a = text.split(" ");
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (word === (a[i].toLowerCase())) {
+        count++;
+      }
+      return this.count;
+    }
   }
-
   occurrences(word) {
     // TODO: return the number of occurrences
+    
   }
 }
 
