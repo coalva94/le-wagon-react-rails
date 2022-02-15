6c9812fc8d218ea888533c699e8922f2d57454a6
coalva94 <coalva.94@gmail.com>
2022-02-15 11:29:18 -0500

    cambios

--- React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,16 @@
 function insertDash(word) {
-  // TODO: implement the method and return word with dashes
-  return null;
+  const pattern = /[^a|e|i|o|u|\s]{2}/gim;
+  const splitWord = word.split(pattern);
+  const dashedConsonant = word.match(pattern);
+  if (!dashedConsonant) return word;
+  const fullWord = [];
+  let count = 0;
+  for (let i = 0; i < dashedConsonant.length; i += 1) {
+    fullWord.push(splitWord[count]);
+    fullWord.push(`${dashedConsonant[count][0]}-${dashedConsonant[count][1]}`);
+    count += 1;
+  }
+  fullWord.push(splitWord.slice(-1));
+  return fullWord.join('');
 }
-
 module.exports = insertDash;
--- React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,10 @@
 function occurrences(text, word) {
-  return null;
+  const a = text.split(" ");
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (word === (a[i].toLowerCase()))
+      count++;
+  }
+  return count;
 }
-
 module.exports = occurrences;
